efb7d80d9714efdb98ca45acdd01e212da069b3b
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-09-26 15:19:10 -0600

    changed bg image, and colors

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -19,7 +19,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div className="WeatherForecastDay">
       <div className="WeatherForecast-day">{day()}</div>
-      <img className="icon-sm" src={props.icon} alt="weather icon" />
+      <img className="icon-sm" src={props.icon} alt={props.data.description} />
       <div className="WeatherForecast-temp">
         <span className="WeatherForecast-temp-max">{maxTemp()}°</span>
         <span className="WeatherForecast-temp-min">{minTemp()}°</span>
