7f624c53ac89a4cf74125756b60b2c9907f18b9c
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-10-03 10:18:58 -0600

    added sky npm component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import StarSky from "react-star-sky";
+import "react-star-sky/dist/index.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
 import SearchForm from "./SearchForm";
 import Alerts from "./Alerts";
@@ -10,7 +11,16 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <div className="container main-box background-images">
+      <div className="container main-box ">
+        <StarSky
+          pageBackground={true}
+          style={{ opacity: 0.5 }}
+          shuffle={true}
+          frameRate={60}
+          className={("container", "main-box")}
+          starColor={(103, 245, 249)}
+          backgroundColor={[56, 40, 92]}
+        />
         <SearchForm defaultCity="Denver" />
       </div>
       <Alerts />
