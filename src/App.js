81d53a1934349560516462cd11597583f3d390f7
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-10-03 12:30:16 -0600

    added background npm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { startTransition } from "react";
+import StarSky from "react-star-sky";
+import "react-star-sky/dist/index.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
 import SearchForm from "./SearchForm";
 import Alerts from "./Alerts";
 import Footer from "./Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <div className="container main-box background-images">
+      <div className="container main-box ">
+        <StarSky
+          pageBackground={true}
+          style={{ opacity: 0.5 }}
+          shuffle={true}
+          frameRate={20}
+          className={("container", "main-box")}
+          starColor={(103, 245, 249)}
+          backgroundColor={[56, 40, 92]}
+        />
         <SearchForm defaultCity="Denver" />
       </div>
       <Alerts />
@@ -18,5 +28,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
