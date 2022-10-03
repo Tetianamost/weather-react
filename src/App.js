d5753b0049ea869b750f354d94118266fc7d17b7
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-10-03 12:47:24 -0600

    Revert "added background npm"
    This reverts commit 81d53a1934349560516462cd11597583f3d390f7.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, { startTransition } from "react";
-import StarSky from "react-star-sky";
-import "react-star-sky/dist/index.css";
+import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 import SearchForm from "./SearchForm";
 import Alerts from "./Alerts";
 import Footer from "./Footer";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <div className="container main-box ">
-        <StarSky
-          pageBackground={true}
-          style={{ opacity: 0.5 }}
-          shuffle={true}
-          frameRate={20}
-          className={("container", "main-box")}
-          starColor={(103, 245, 249)}
-          backgroundColor={[56, 40, 92]}
-        />
+      <div className="container main-box background-images">
         <SearchForm defaultCity="Denver" />
       </div>
       <Alerts />
@@ -28,3 +18,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
