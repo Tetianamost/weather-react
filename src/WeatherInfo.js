7ef85fc39072c5afdf0408da4faa00b4e1ad0c26
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-10-03 20:38:54 -0600

    no card text

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,7 +11,7 @@ export default function WeatherInfo(props) {
       <div className="d-flex">
         <ul className="tab card text-center my-3">
           <li className="h1">{props.data.city}</li>
-          <li className="h6 card-text">{props.data.description}</li>
+          <li className="h6">{props.data.description}</li>
         </ul>
       </div>
       <div className="row weatherInfo">
